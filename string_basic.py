f22ea9cfdc127c31f202ae7ebb9a4e30d87ea684
wt-p <wtbtty9955@gmail.com>
2024-11-25 02:37:00 +0900

    first commit

---
+++ string_basic.py
@@ -0,0 +1,20 @@
+# 文字列(Strings)
+
+print("Hello world!!")
+print('Hello world!!')
+print('1')
+print("I'm file")
+print('I"m file')
+print("""
+Hello world!!
+How are you doing?
+""")
+print('''
+Hello world
+a
+''')
+print("hello \nworld")
+print("hello \tworld")
+print("back slash n:\\n")
+print('I\'m fine')
+print("hello" + "world" + "!!")
